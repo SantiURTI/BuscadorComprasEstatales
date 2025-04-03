e4c07355d948a4a62b1c16b2bba46400abc0437e
Santiago Urtiaga <81543262+SantiURTI@users.noreply.github.com>
2025-04-03 16:09:03 -0300

    Create fetch-data.js

---
+++ fetch-data.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.comprasestatales.gub.uy/consultas/', {
+    waitUntil: 'networkidle2',
+  });
+
+  // Scroll hasta el final de la página para cargar todas las compras
+  await autoScroll(page);
+
+  // Obtener el contenido HTML de la página
+  const content = await page.content();
+
+  // Guardar el contenido HTML en un archivo
+  fs.writeFileSync('data.html', content);
+
+  await browser.close();
+})();
+
+async function autoScroll(page){
+  await page.evaluate(async () => {
+      await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+              var scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+
+              if(totalHeight >= scrollHeight - window.innerHeight){
+                  clearInterval(timer);
+                  resolve();
+              }
+          }, 100);
+      });
+  });
+}
